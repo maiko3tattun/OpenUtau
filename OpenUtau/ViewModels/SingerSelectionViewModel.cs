6e3d1388db83294474b7c2112ae99a6c4558f0e9
Maiko <maiko3tattun@gmail.com>
2023-07-25 18:07:26 +0900

    wip singer selection dialog

---
+++ OpenUtau/ViewModels/SingerSelectionViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DynamicData.Binding;
+using OpenUtau.Classic;
+using OpenUtau.Core;
+using OpenUtau.Core.Ustx;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using SharpCompress.Archives;
+using SharpCompress.Common;
+using SharpCompress.Readers;
+
+namespace OpenUtau.App.ViewModels {
+    public class SingerSelectionViewModel : ViewModelBase {
+        [Reactive] public ObservableCollection<string> Categories { get; set; } = new ObservableCollection<string>();
+        [Reactive] public ObservableCollection<USinger> ClassicList { get; set; } = new ObservableCollection<USinger>();
+        [Reactive] public ObservableCollection<USinger> ENUNUList { get; set; } = new ObservableCollection<USinger>();
+        [Reactive] public string ClassicHeader { get; set; } = "Classic";
+        [Reactive] public string ENUNUHeader { get; set; } = "ENUNU";
+        [Reactive] public int SelectedCategory { get; set; } = 0;
+        [Reactive] USinger? SelectedSinger { get; set; }
+        [Reactive] string Search { get; set; } = "";
+        [Reactive] bool LoadAllFolders { get; set; } = true;
+
+        public IEnumerable<USinger> Singers => SingerManager.Inst.SingerGroups.Values.SelectMany(l => l);
+
+        public SingerSelectionViewModel() {
+
+        }
+    }
+}
---
+++ OpenUtau/Views/SingerSelectionDialog.axaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Platform.Storage;
+using NWaves.Audio;
+using OpenUtau.App.ViewModels;
+using OpenUtau.Core;
+using OpenUtau.Core.Ustx;
+using Serilog;
+
+namespace OpenUtau.App.Views {
+    public partial class SingerSelectionDialog : Window {
+        public SingerSelectionDialog() {
+            InitializeComponent();
+        }
+    }
+}
